104c0f968a7569333f17855f027d0ce4b5aa240e
BedivereZero <BedivereZero@gmail.com>
2018-06-13 16:10:51 +0800

    save weibo name & userid in peropero.users

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import sqlite3
 
 API = 'http://m.weibo.cn/container/getIndex'
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'}
+DBNAME = 'peropero.sqlite'
 
 
 def create_database(path):
@@ -20,21 +21,23 @@ def create_database(path):
 
 def main():
     """pass"""
-    weiboname = '咸鱼面儿'
+    weiboname = u'咸鱼面儿'
     # create database
-    dbpath = os.path.abspath('peropero.sqlite')
+    dbpath = os.path.abspath(DBNAME)
     if not os.path.exists(dbpath):
         create_database(dbpath)
     workpath = os.path.abspath(os.path.join('.', weiboname))
     if not os.path.exists(workpath):
         os.mkdir(workpath)
-    res = requests.get(url='http://m.weibo.cn/n/{name}'.format(name=weiboname))
+    res = requests.get(url='http://m.weibo.cn/n/{name}'.format(name=weiboname.encode('utf-8')))
     cookie = res.headers.get('Set-Cookie')
     fid = int(re.match(r'^fid%3D(\d+)%26uicode%3D\d+', res.cookies.get('M_WEIBOCN_PARAMS')).group(1))
     userid = int(re.match(r'https://m.weibo.cn/u/(\d+)', res.url).group(1))
     # insert into peropero
-    print(fid)
-    return 0
+    with sqlite3.connect(DBNAME) as cnx:
+        cur = cnx.execute('SELECT * FROM users WHERE userid = {id}'.format(id=userid))
+        if not cur.fetchall():
+            cnx.execute('INSERT INTO users VALUES(?, ?)', (userid, weiboname))
     res = requests.get(url=API, params=dict(containerid=fid))
     cont = res.json()
     tabs = cont['data']['tabsInfo']['tabs']
@@ -60,7 +63,6 @@ def main():
                 if 'retweeted_status' in card['mblog']:
                     # skip retweet
                     continue
-                # print(card['mblog'].get('mid'), 'pics' in card['mblog'])
                 if 'pics' not in card['mblog']:
                     continue
                 print('parsering {text}'.format(text=card['mblog'].get('raw_text')))
