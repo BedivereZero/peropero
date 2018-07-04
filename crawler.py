50672ec09b03311555d01af736c85cc070a935c2
BedivereZero <BedivereZero@gmail.com>
2018-07-04 15:03:09 +0800

    use crawler thread instead

--- crawler.py
+++ crawler.py
@@ -1,22 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import requests
-import threading
 import logging
+import os
+import re
+import requests
+import sqlite3
+import threading
 
-from utils.database import DBNAME
+DBNAME = 'peropero.sqlite'
 
 
 logger = logging.getLogger(__name__)
 
-logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s', level=logging.DEBUG)
+# logging.basicConfig(format='%(asctime)s %(name)s %(levelname)s %(message)s', level=logging.DEBUG)
 
 
-class Crawler(threading.Thread):
+class CrawlerThread(threading.Thread):
     """ crawler thread """
 
-    API = 'http://m.weibo.cn/container/getIndex'
+    API = 'https://m.weibo.cn/container/getIndex'
     HEADERS = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36',
     }
@@ -30,26 +33,20 @@ class Crawler(threading.Thread):
 
     def run(self):
         """ run """
-        logger.debug('checking user directory')
-        user_dir = os.path.abspath(self.weiboname)
-        if not os.path.exists(user_directory):
-            logger.debug('create user directory {path}'.format(path=user_dir))
-            os.mkdir(user_dir)
-
-        logger.debug('getting userid from weiboname')
-        rsp = requests.get(url='http:/m.weibo.cn/n/{}'.format(weiboname.encode('utf_8')))
+        logger.info('getting userid from weiboname')
+        rsp = requests.get(url='https://m.weibo.cn/n/{}'.format(self.weiboname.encode('utf_8')))
         fid = int(re.match(r'^fid%3D(\d+)%26uicode%3D\d+', rsp.cookies.get('M_WEIBOCN_PARAMS')).group(1))
         self.userid = int(re.match(r'https://m.weibo.cn/u/(\d+)', rsp.url).group(1))
 
-        logger.debug('checking user information')
+        logger.info('adding user information')
         with sqlite3.connect(DBNAME) as cnx:
             cur = cnx.execute('SELECT `name` FROM users WHERE `userid` = ?', (self.userid,))
             row = cur.fetchone()
             if not row:
-                logger.debug('create user record {}:{}'.format(self.weiboname, self.userid))
-                cnx.execute('INSERT INTO users VALUES(?, ?)', (self.userid, self.weiboname)
+                logger.info('create user record {}:{}'.format(self.weiboname.encode('utf_8'), self.userid))
+                cnx.execute('INSERT INTO users VALUES(?, ?)', (self.userid, self.weiboname))
 
-        logger.debug('getting weibo container')
+        logger.info('getting weibo container')
         rsp = requests.get(url=self.API, params=dict(containerid=fid))
         dat = rsp.json()
         for tab in dat['data']['tabsInfo']['tabs']:
@@ -57,12 +54,26 @@ class Crawler(threading.Thread):
                 cid = int(tab.get('containerid'))
                 break
 
-        logger.debug('getting pages')
+        logger.info('getting pages')
         page = 1
         rsp = requests.get(url=self.API, headers=self.HEADERS, params=dict(containerid=cid))
         dat = rsp.json()
         while dat.get('ok'):
-            pass
+            for card in dat['data']['cards']:
+                import json
+                if card['card_type'] is 11:
+                    logger.debug('skip: card_type = 11')
+                    continue
+                if 'retweeted_status' in card['mblog']:
+                    logger.debug('skip: retweeted')
+                for pic in card['mblog'].get('pics', list()):
+                    with sqlite3.connect(DBNAME) as cnx:
+                        cur = cnx.execute('SELECT imageid FROM images WHERE imageid = ?', (pic['pid'],))
+                        if cur.fetchone():
+                            logger.debug('image {pid} has been seen'.format(pid=pic['pid']))
+                        else:
+                            logger.info('adding image {pid}'.format(pid=pic['pid']))
+                            cur.execute('INSERT INTO images(imageid, userid, url) VALUES(?, ?, ?)', (pic['pid'], self.userid, pic['large']['url']))
             page += 1
             rsp = requests.get(url=self.API, headers=self.HEADERS, params=dict(containerid=cid, page=page))
             dat = rsp.json()
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import json
 import logging
 import os
-import re
 import requests
 import sqlite3
-import threading
 
-API = 'http://m.weibo.cn/container/getIndex'
-HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.84 Safari/537.36'}
+from crawler import CrawlerThread
+
 DBNAME = 'peropero.sqlite'
 
+logger = logging.getLogger(__name__)
 
 
 def create_database(path):
@@ -31,7 +29,7 @@ def download_image(workpath, picture):
             headers=HEADERS,
         )
     except requests.exceptions.RequestException as ex:
-        logging.info('download image {url} failed, {ex}'.format(url=picture['large']['url'], ex=ex))
+        logger.info('download image {url} failed, {ex}'.format(url=picture['large']['url'], ex=ex))
         download = False
     else:
         download = True
@@ -51,7 +49,7 @@ def download_image(workpath, picture):
                 picture['pid'],
             ),
         )
-    logging.info('download image {url} succeed'.format(url=picture['large']['url']))
+    logger.info('download image {url} succeed'.format(url=picture['large']['url']))
 
 def main():
     """pass"""
@@ -62,83 +60,14 @@ def main():
     if not os.path.exists(dbpath):
         create_database(dbpath)
 
-    # create work directory
-    workpath = os.path.abspath(os.path.join('.', weiboname))
-    if not os.path.exists(workpath):
-        os.mkdir(workpath)
-    res = requests.get(url='http://m.weibo.cn/n/{name}'.format(name=weiboname.encode('utf_8')))
-    fid = int(re.match(r'^fid%3D(\d+)%26uicode%3D\d+', res.cookies.get('M_WEIBOCN_PARAMS')).group(1))
-    userid = int(re.match(r'https://m.weibo.cn/u/(\d+)', res.url).group(1))
+    # start crawler thread
+    crawler_thread = CrawlerThread(weiboname=weiboname)
+    crawler_thread.start()
 
-    # insert into peropero
-    with sqlite3.connect(DBNAME) as cnx:
-        cur = cnx.execute('SELECT * FROM users WHERE userid = {id}'.format(id=userid))
-        if not cur.fetchall():
-            cnx.execute('INSERT INTO users VALUES(?, ?)', (userid, weiboname))
-
-    # get weibo container
-    res = requests.get(url=API, params=dict(containerid=fid))
-    cont = res.json()
-    tabs = cont['data']['tabsInfo']['tabs']
-    for tab in cont['data']['tabsInfo']['tabs']:
-        if tab['tab_type'] == 'weibo':
-            cid = tab.get('containerid')
-
-    # get page
-    page = 1
-    res = requests.get(
-        url=API,
-        headers=HEADERS,
-        params=dict(containerid=cid),
-    )
-    cont = res.json()
-    while cont.get('ok') == 1:
-        data = cont.get('data')
-        if data.get('cards'):
-            for card in data.get('cards'):
-                if card['card_type'] == 11:
-                    # what's meaning of card_type = 11
-                    # return 302
-                    logging.debug('skip: card_type = 11')
-                    continue
-                if 'retweeted_status' in card['mblog']:
-                    # skip retweet
-                    logging.debug('skip: retweeted')
-                    continue
-                if 'pics' not in card['mblog']:
-                    continue
-                for pic in card['mblog']['pics']:
-                    # insert image info into database if image has not been seen
-                    with sqlite3.connect(DBNAME) as cnx:
-                        cur = cnx.execute('SELECT imageid FROM images WHERE imageid = ?', (pic['pid'],))
-                        if cur.fetchone():
-                            logging.info('image {pid} has been seen'.format(pid=pic['pid']))
-                        else:
-                            logging.info('adding image {pid}'.format(pid=pic['pid']))
-                            cnx.execute(
-                                'INSERT INTO images(imageid, userid, url) VALUES(?, ?, ?)',
-                                (
-                                    pic['pid'],
-                                    userid,
-                                    pic['large']['url'],
-                                ),
-                            )
-        else:
-            break
-        page += 1
-        res = requests.get(
-            url=API,
-            headers=HEADERS,
-            params=dict(
-                containerid=cid,
-                page=page,
-            ),
-        )
-        cont = res.json()
 
 if __name__ == '__main__':
     logging.basicConfig(
-        format='%(asctime)s %(levelname)s %(message)s',
+        format='%(asctime)s %(name)s %(levelname)s %(message)s',
         level=logging.INFO,
     )
     main()
