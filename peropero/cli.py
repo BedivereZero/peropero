98da3e4e3ab9fab85576df8591978f934098e5cf
BedivereZero <BedivereZero@gmail.com>
2019-10-22 00:30:54 +0800

    Add unittest of peropero.cli
    Signed-off-by: BedivereZero <BedivereZero@gmail.com>

--- peropero/cli.py
+++ peropero/cli.py
@@ -1,27 +1,42 @@
 # command line interface
 
-from argparse import ArgumentParser
-from logging import DEBUG
-from logging import Formatter
-from logging import INFO
-from logging import StreamHandler
-from logging import getLogger
+import argparse
+import logging
 
-FORMAT = '%(levelname)-.3s %(asctime)s.%(msecs)03d %(name)s: %(message)s'
-DATEFORMAT = '%Y%m%d.%H%M%S'
-
-
-formatter = Formatter(FORMAT, DATEFORMAT)
-handler = StreamHandler()
-handler.setFormatter(formatter)
-logger = getLogger(__name__)
-logger.addHandler(handler)
+from peropero.creeper.weibo import WeiboCreeper
+from peropero import __version__
 
 
 def main():
     """main"""
+    fmt = '%(levelname)-.3s %(asctime)s.%(msecs)03d %(name)s: %(message)s'
+    datefmt = '%Y%m%d.%H%M%S'
+    formatter = logging.Formatter(fmt, datefmt)
+    handler = logging.FileHandler('peropero.log')
+    handler.setFormatter(formatter)
+    logger = logging.getLogger('peropero')
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
 
-    parser = ArgumentParser(description='peropero')
-    parser.add_argument('site', help='site')
-    parser.add_argument('name', help='name')
+    parser = argparse.ArgumentParser(description='peropero')
+    parser.add_argument('--version', action='store_true', help='version')
+
+    subparsers = parser.add_subparsers(help='sub-command help')
+
+    parser_weibo = subparsers.add_parser('weibo', help='weibo')
+    parser_weibo.set_defaults(creeper=WeiboCreeper)
+    parser_weibo.add_argument('name', nargs='+', help='names')
+
+    parser_twitter = subparsers.add_parser('twitter', help='twitter')
+    parser_twitter.set_defaults(creeper=lambda s: print(s))
+    parser_twitter.add_argument('name', nargs='+', help='names')
+
     args = parser.parse_args()
+
+    if args.version:
+        print(__version__)
+        return
+
+    creeper = args.creeper(args.name)
+    for pid in creeper.pids:
+        print(pid)
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,16 +3,32 @@
 
 import logging
 import sys
+import unittest
+import unittest.mock
 
-from unittest import TestCase
-from unittest import mock
+from peropero.cli import main
 
 
-class TestCli(TestCase):
+class TestCli(unittest.TestCase):
 
     def setUp(self):
         self.logger = logging.getLogger(__name__)
         sys.argv = list()
+        patcher = unittest.mock.patch('peropero.cli.WeiboCreeper')
+        self.mock_weibo_creeper = patcher.start()
+        self.addCleanup(patcher.stop)
 
     def test_hello(self):
         self.logger.info('hello')
+        self.mock_weibo_creeper.pids = []
+        sys.argv = [
+            'peropero',
+            'weibo',
+            'username',
+        ]
+        main()
+        self.mock_weibo_creeper.assert_called_once_with('username')
+
+
+if __name__ == '__main__':
+    unittest.main()
